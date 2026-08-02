42fa530856e103f141114cba0d56c07c641da148
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-02 12:37:20 +0530

    Update tool to be redesigned

--- expense_tracker_agent/database.py
+++ expense_tracker_agent/database.py
@@ -31,17 +31,40 @@ class DatabaseManager:
     def get_expense_by_category(self, category):
         self.cursor.execute("select * from expense where category = ?", (category,))
         return self.cursor.fetchall()
-
-    def update_expense(self, expense_id, amount, category, description, expense_date):
+    
+    def update_expense(self, expense_id = None, amount = None, category = None, description = None, expense_date = None):
         self.cursor.execute("""
         Update expense set amount = ?, category = ?, description = ?, expense_date = ? where expense_id = ?
 """, (amount, category, description, expense_date, expense_id))
 
         self.connection.commit()
 
-    def delete_expense(self, expense_id):
-        self.cursor.execute("delete from expense where id = ?", (expense_id))
+    def delete_expense(self, expense_id = None, amount = None, category = None, description = None, expense_date = None):
+        query = "delete from expense where 1=1"
+        params = []
 
+        if expense_id is not None:
+            query+=" And id = ?"
+            params.append(expense_id)
+
+        if amount is not None:
+            query+=" And amount = ?"
+            params.append(amount)
+        
+        if category is not None:
+            query+=" And category = ?"
+            params.append(category)
+        
+        if description is not None:
+            query+=" And description Like ?"
+            params.append(description)
+        
+        if expense_date is not None:
+            query+=" And expense_date = ?"
+            params.append(expense_date)
+
+        
+        self.cursor.execute(query, params)
         self.connection.commit()
 
     def close(self):
