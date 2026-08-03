9653462dbbe435e55372981706f5998ca2c00230
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-03 21:18:41 +0530

    Flight search helper functions all done

--- travel_planner_agent/flight_search.py
+++ travel_planner_agent/flight_search.py
@@ -11,7 +11,23 @@ HEADERS = {
 }
 
 class flight_search:
-    def city_lookup():
+    def make_request(self, endpoint: str, params: dict | None = None):
+        response = requests.get(
+            BASE_URL + endpoint,
+            headers=HEADERS,
+            params=params,
+            timeout=30
+        )
+
+        response.raise_for_status()
+
+        result = response.json()
+
+        if not result.get("success", True):
+            raise Exception(result.get("error"))
+
+        return result
+    def city_lookup(self):
         response = requests.get(
             "https://api.travelpayouts.com/data/en/cities.json",
             headers=HEADERS,
@@ -21,3 +37,109 @@ class flight_search:
         response.raise_for_status()
 
         return response.json()
+
+    def airport_lookup(self):
+        response = requests.get(
+        "https://api.travelpayouts.com/data/en/airports.json",
+        headers=HEADERS,
+        timeout=30
+    )
+        response.raise_for_status()
+
+        return response.json()
+
+    def airline_lookup(self):
+        response = requests.get(
+        "https://api.travelpayouts.com/data/en/airlines.json",
+        headers=HEADERS,
+        timeout=30
+    )
+        response.raise_for_status()
+
+        return response.json()
+
+    def search_direct_flights(
+            self,
+            origin,
+    destination,
+    depart_date,
+    return_date=None,
+    currency="INR"
+    ):
+        params = {
+            "origin": origin,
+            "destination": destination,
+            "depart_date": depart_date,
+            "currency": currency
+        }
+
+        if return_date:
+            params["return_date"] = return_date
+
+        return self.make_request(
+            "/v1/prices/direct",
+            params
+        )
+    def search_calender_prices(
+            self,
+            origin,
+            destination,
+            depart_date,
+            calendar_type="departure_date",
+            return_date=None,
+            length=None,
+            currency="INR"
+        ):
+
+        params = {
+            "origin": origin,
+        "destination": destination,
+        "depart_date": depart_date,
+        "calendar_type": calendar_type,
+        "currency": currency
+        }
+
+        if return_date:
+            params["return_date"] = return_date
+
+        if length:
+            params["length"] = length
+
+        return self.make_request(
+            "/v1/prices/calendar",
+            params
+        )
+
+    def search_monthly_prices(
+            self,
+            origin,
+            destination,
+            currency="INR"
+        ):
+
+        params = {
+            "origin": origin,
+            "destination": destination,
+            "currency": currency
+        }
+
+        return self.make_request(
+            "/v1/prices/monthly",
+            params
+        )
+
+    def popular_destination(
+            self,
+            origin,
+            currency = "INR"
+        ):
+
+        params = {
+            "origin": origin,
+            "currency": currency
+        }
+
+        return self.make_request(
+            "/v1/city-directions",
+            params
+        )
